6676390f9d590c78aa91126a3f7856fa02030510
student <1>
2022-03-09 11:51:23 -0500

    CW changes

---
+++ src/com/company/Limerick.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Limerick extends Poem {
+    public Limerick() {
+        super(5);
+    }
+
+    public int getSyllables(int k) {
+        if (k == 1 || k == 2 || k == 5) {
+            return 9;
+        } else {
+            return 6;
+        }
+    }
+
+    public void printRhythm() {
+        for (int i = 1; i <= getNumlines(); i++) {
+            for (int j = 0; j < getSyllables(i); j++) {
+                System.out.print("ta");
+                if (j != getSyllables(i)-1) {
+                    System.out.print("-");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
--- src/com/company/Poem.java
+++ src/com/company/Poem.java
@@ -1,7 +1,18 @@
 package com.company;
 
 public class Poem {
+    private int lines;
 
+    public Poem(int l){
+        lines=l;
+    }
 
+    public int getNumlines(){
+        return lines;
+    }
+
+    public void printRhythm(){
+        System.out.println("Free verse!");
+    }
 
 }
